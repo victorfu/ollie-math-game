2ae029e22e8daeb412ce0ddaa7243001b126f8a5
Victor Fu <supergothere@gmail.com>
2025-03-29 18:26:37 +0800

    fix: deploy on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/math-game/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+  },
 });
