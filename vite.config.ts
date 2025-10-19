9ea4f08bbf64a62809b4bc32e5c87249d44bd8bc
Victor Fu <supergothere@gmail.com>
2025-10-19 09:17:31 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/math-game/",
+  base: "/ollie-math-game/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
