f8ddd6904219a1e5089bb48aea078064fafedbd8
Victor Fu <supergothere@gmail.com>
2025-10-19 09:15:27 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/math-game">
+    <BrowserRouter basename="/ollie-math-game">
       <App />
     </BrowserRouter>
   </StrictMode>,
